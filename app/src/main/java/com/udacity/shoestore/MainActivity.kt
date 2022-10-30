52c74dd486a1668bdaae7209891a13bbdffef5f7
Daniel Vidales <daniel.vidales@dexcom.com>
2022-10-29 18:00:23 -0700

    ACTION_BAR_NAVIGATION. Add styles, AndroidManifest setup needed for Navigation

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,15 +3,32 @@ package com.udacity.shoestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration:AppBarConfiguration
         private var _binding: ActivityMainBinding? = null
         private val binding: ActivityMainBinding get() = binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding =DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        val navController = this.findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+
         Timber.plant(Timber.DebugTree())
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
