e0af42e20d4c6167d600316492029dba79712ac5
Daniel Vidales <daniel.vidales@dexcom.com>
2022-10-30 03:05:21 -0700

    SHOE_DETAIL_FRAGMENT. Initial version of FragmentShoeDetail implementation

---
+++ app/src/main/java/com/udacity/shoestore/models/ShoeStoreViewModel.kt
@@ -0,0 +1,15 @@
+package com.udacity.shoestore.models
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ShoeStoreViewModel: ViewModel() {
+
+val _shoeList = MutableLiveData<MutableList<Shoe>>(mutableListOf<Shoe>())
+val shoeList: LiveData<MutableList<Shoe>> get() = _shoeList
+
+    fun addShoe(shoe:Shoe?){
+        shoe?.let{shoeList.value?.add(it)}
+    }
+}
