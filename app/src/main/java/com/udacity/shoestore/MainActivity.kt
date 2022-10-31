3689b4d5d84cef6493a8f0bbca20013eee19b106
Daniel Vidales <daniel.vidales@dexcom.com>
2022-10-31 15:29:34 -0700

    SHOE_LIST_FRAGMENT_UP_NAVIGATION_PREV. Updates required on MainActivity in order to prevent ubackwards Navigation once on ShoeListFragment

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,34 +1,56 @@
 package com.udacity.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.udacity.shoestore.databinding.ActivityMainBinding
+import com.udacity.shoestore.models.ShoeStoreViewModel
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration:AppBarConfiguration
-        private var _binding: ActivityMainBinding? = null
-        private val binding: ActivityMainBinding get() = binding!!
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private var _binding: ActivityMainBinding? = null
+    private lateinit var shoeStoreViewModel: ShoeStoreViewModel
+    var shoeListFragmentActive = false
+    private lateinit var navController: NavController
+
+    private val binding: ActivityMainBinding get() = binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding =DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+        shoeStoreViewModel = ViewModelProvider(this).get(ShoeStoreViewModel::class.java)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+/*
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+            navController = navHostFragment.findNavController()
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+*/
 
         val navController = this.findNavController(R.id.navHostFragment)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
 
-
         Timber.plant(Timber.DebugTree())
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.navHostFragment)
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
+        if (!shoeStoreViewModel.shoeListFragmentActive) {
+            val navController = this.findNavController(R.id.navHostFragment)
+            return NavigationUI.navigateUp(navController, appBarConfiguration)
+        }
+        return true
     }
+
+    override fun onBackPressed() {
+        if (!shoeStoreViewModel.shoeListFragmentActive) {
+            super.onBackPressed()
+        }
+    }
+
 }
